80afba43b1d6774aeb5e1a5cfd2e295149c4fd69
Ghazi Triki <ghazi.triki@riadvice.tn>
2024-06-19 19:49:04 +0100

    Remove guanguans/notify dependency and send Zulip message using F3 Web class.

--- src/Sukarix/Notification/Notifier.php
+++ src/Sukarix/Notification/Notifier.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Sukarix\Notification;
 
-use Guanguans\Notify\Factory;
-use Guanguans\Notify\Messages\Zulip\StreamMessage;
 use Sukarix\Behaviours\HasF3;
 use Sukarix\Behaviours\LogWriter;
 use Sukarix\Configuration\Environment;
@@ -29,21 +27,38 @@ class Notifier extends Tailored
             $message = 'An error occurred on <b>' . Environment::getHostName() . '</b><br />' . nl2br($exception->getTraceAsString());
             $host    = Environment::getHostName();
 
-            Factory::zulip()
-                ->setToken($this->f3->get('NOTIFICATIONS.zulip.token'))
-                ->setEmail($this->f3->get('NOTIFICATIONS.zulip.mail'))
-                ->setBaseUri($this->f3->get('NOTIFICATIONS.zulip.uri'))
-                ->setMessage(
-                    new StreamMessage(
-                        [
-                            'to'      => $this->f3->get('NOTIFICATIONS.zulip.stream'),
-                            'topic'   => $this->f3->get('NOTIFICATIONS.zulip.topic'),
-                            'content' => "An error occurred on host [https://{$host}](https://{$host})\n* **Error ID:** {$hash}\n* **Subject:** {$subject}\n* **Message:** {$message}",
-                        ]
-                    )
-                )
-                ->send()
-            ;
+            $this->zulip(['content' => "An error occurred on host [https://{$host}](https://{$host})\n* **Error ID:** {$hash}\n* **Subject:** {$subject}\n* **Message:** {$message}"]);
         }
+    }
+
+    /**
+     * Sends a message to Zulip using F3 Web instance.
+     *
+     * @param array $params The parameters for the Zulip message.
+     *                      Possible keys:
+     *                      - 'type': The type of message to send. Possible values: 'stream', 'private', 'direct' and 'channel'.
+     *                      - 'to': The destination of the message. For 'stream' type, this is the stream name.
+     *                      - 'topic': The topic of the message. Required for 'stream' or 'channel") type.
+     *                      - 'content': The content of the message.
+     *                      - 'queue_id': The ID of the queue to add the message to.
+     *                      - 'local_id': A unique local identifier for the message.
+     *                      - 'read_by_sender': Whether the message should be initially marked read by its sender.
+     *
+     * @return array|false the response from the Zulip API
+     */
+    public function zulip(array $params = []): array|false
+    {
+        return \Web::instance()->request(rtrim($this->f3->get('NOTIFICATIONS.zulip.uri'), '/') . '/api/v1/messages', [
+            'method' => 'POST',
+            'header' => [
+                'Content-Type: application/x-www-form-urlencoded',
+                'Authorization: Basic ' . base64_encode($this->f3->get('NOTIFICATIONS.zulip.mail') . ':' . $this->f3->get('NOTIFICATIONS.zulip.token')),
+            ],
+            'content' => http_build_query(array_merge([
+                'type'  => 'stream',
+                'to'    => $this->f3->get('NOTIFICATIONS.zulip.stream'),
+                'topic' => $this->f3->get('NOTIFICATIONS.zulip.topic'),
+            ], $params)),
+        ]);
     }
 }
