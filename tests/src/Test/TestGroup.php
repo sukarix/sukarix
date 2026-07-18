656ea0787f3601e831cf3d2f0518ad3b5652355d
Ghazi Triki <ghazi.triki@riadvice.com>
2026-07-18 15:48:38 +0100

    Add TestGroup class for statera test grouping

---
+++ tests/src/Test/TestGroup.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test;
+
+use Sukarix\TestGroup as SukarixTestGroup;
+
+/**
+ * Base test group for the Sukarix library test suite.
+ *
+ * @internal
+ *
+ * @coversNothing
+ */
+class TestGroup extends SukarixTestGroup {}
