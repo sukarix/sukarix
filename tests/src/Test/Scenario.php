d6818b58621f9721fa0c9c62373aaebc84170650
Ghazi Triki <ghazi.triki@riadvice.com>
2026-07-18 15:48:38 +0100

    Add Scenario class for statera test scenarios

---
+++ tests/src/Test/Scenario.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test;
+
+use Sukarix\TestScenario as SukarixTestScenario;
+
+/**
+ * Base test scenario for the Sukarix library test suite.
+ *
+ * @internal
+ *
+ * @coversNothing
+ */
+class Scenario extends SukarixTestScenario {}
