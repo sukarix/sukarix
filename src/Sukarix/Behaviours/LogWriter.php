cfcb2d4527b86ef2322cbb324b5b180fadc822d1
Ghazi Triki <ghazi.triki@riadvice.com>
2026-07-18 15:47:10 +0100

    Improve LogWriter with configurable log file and level handling

--- src/Sukarix/Behaviours/LogWriter.php
+++ src/Sukarix/Behaviours/LogWriter.php
@@ -19,17 +19,28 @@ trait LogWriter
 
     public function initLogWriter(): void
     {
+        $f3       = \Base::instance();
+        $logFile  = $f3->get('application.logfile') ?: ($f3->get('LOGS') . 'app.log');
+        $level    = mb_strtoupper($f3->get('log.level') ?: 'info');
+        $constant = (new \ReflectionClass(Logger::class))->getConstants()[$level] ?? Logger::INFO;
+
         $this->logger = new Logger(static::class);
-        $level        = mb_strtoupper(\Base::instance()->get('log.level'));
-        $class        = new \ReflectionClass(Logger::class);
-        $stream       = new StreamHandler(\Base::instance()->get('application.logfile'), $class->getConstants()[$level]);
+        $stream       = new StreamHandler($logFile, $constant);
         $stream->setFormatter(
             new LineFormatter('[' . (\Base::instance()->ip() ?: 'CLI:PID.' . getmypid()) . '] '
-                . '[%datetime%] [' . \Base::instance()->get('VERB') . (true === \Base::instance()->get('AJAX') ? '.AJAX' : '') . '] %channel%.%level_name%: %message% %context% %extra%' .
-                ' [' . \Base::instance()->get('AGENT') . ']' .
+                . '[%datetime%] [' . \Base::instance()->get('VERB') . (true === \Base::instance()->get('AJAX') ? '.AJAX' : '') . '] %channel%.%level_name%: %message% %context% %extra%'
+                . ' [' . \Base::instance()->get('AGENT') . ']'
                 // Keep line return between double quotes
-                "\n", 'Y-m-d G:i:s.u')
+                . "\n", 'Y-m-d G:i:s.u')
         );
         $this->logger->pushHandler($stream);
+    }
+
+    /**
+     * Backward-compatible alias for initLogWriter().
+     */
+    public function initLogger(): void
+    {
+        $this->initLogWriter();
     }
 }
